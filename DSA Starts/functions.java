a8b42ad6a333026bdd9455749dbbd7f33128a686
saurabh407-tech <sourabhshukla8318@gmail.in>
2025-09-20 18:29:15 +0530

    first

--- DSA Starts/functions.java
+++ DSA Starts/functions.java
@@ -1,68 +1,69 @@
-// import java.util.*;
+/* import java.util.*;
  
-// public class functions{
-//     public static void printMyName(String name){
-//         System.out.println(name);
-//         return;
-//     }
+public class functions{
+    public static void printMyName(String name){
+        System.out.println(name);
+        return;
+    }
 
-//     public static void main(String[] args){
-//         Scanner sc=new Scanner(System.in);
-//         System.out.println("Enter your name: ");
-//         String name=sc.next();
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name=sc.next();
 
-//         printMyName(name);
-//     }
-// }
-
-/* Que1) Make a function to add 2 numbers and return the sum..*/
-
-// import java.util.*;
-
-// public class functions{
-
-// public static int calculateSum(int a,int b){
-//     int Sum=a+b;
-//     return Sum;
-// }
-//  public static void main(String[] args){
-//     Scanner sc=new Scanner(System.in);
-//     System.out.println("Enter the first number");
-//     int a=sc.nextInt();
-//     System.out.println("Enter the second number");
-//     int b=sc.nextInt();
-
-//     int Sum=calculateSum(a,b);
-
-//     System.out.println("Sum of the numbers is: "+Sum);
-//  }
-// }
+        printMyName(name);
+    }
+} 
 
 
-/* Que2) Make a function to multiply 2 numbers and return the product..*/
+//  Que1) Make a function to add 2 numbers and return the sum.. 
+
+import java.util.*;
+
+public class functions{
+
+public static int calculateSum(int a,int b){
+    int Sum=a+b;
+    return Sum;
+}
+ public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the first number");
+    int a=sc.nextInt();
+    System.out.println("Enter the second number");
+    int b=sc.nextInt();
+
+    int Sum=calculateSum(a,b);
+
+    System.out.println("Sum of the numbers is: "+Sum);
+ }
+}
 
 
-// import java.util.*;
+//  Que2) Make a function to multiply 2 numbers and return the product...
 
-// public class functions{
-//     public static int calculateProduct(int a,int b){
-//      int product=a*b;
-//      return product;
-// }
-//   public static void main(String[] args){
-//     Scanner sc=new Scanner(System.in);
-//       System.out.println("Enter the first number: ");
-//       int a=sc.nextInt();
-//       System.out.println("Enter the second number:");
-//       int b=sc.nextInt();
+
+import java.util.*;
+
+public class functions{
+    public static int calculateProduct(int a,int b){
+     int product=a*b;
+     return product;
+}
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+      System.out.println("Enter the first number: ");
+      int a=sc.nextInt();
+      System.out.println("Enter the second number:");
+      int b=sc.nextInt();
      
-//       int product=calculateProduct(a,b);
-//       System.out.println("The product of the given number is:"+product);
-//   }
-// }
+      int product=calculateProduct(a,b);
+      System.out.println("The product of the given number is:"+product);
+  }
+}               */
 
 
-/* Que3) Find the Factorial of any number.. */
+//  Que3) Find the Factorial of any number.. 
 
 
 import java.util.*;
